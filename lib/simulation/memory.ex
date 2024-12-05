7143469e51ec05ca2a7a2d59e12580ef82148f1e
3rickDJ <erick.dejesus1@alumno.buap.mx>
2024-12-05 12:21:54 -0600

    refactor: enhance logging and structure in Simulation.Memory for LFU page replacement

--- lib/simulation/memory.ex
+++ lib/simulation/memory.ex
@@ -8,11 +8,9 @@ defmodule Simulation.Memory do
   # @reference 0b01000
   # @cache_disabled 0b10000
 
-
   # def run() do
   #   run([0, 4, 1, 4, 2, 4, 3, 4, 2, 4, 0, 4, 1, 4, 2, 4, 3, 4], 3, 5, 1024)
   # end
-
 
   def run(%Simulation.Program{} = program) do
     run(program.references)
@@ -84,8 +82,27 @@ defmodule Simulation.Memory do
               {page_table, memory, frequency_table}
           end
 
-        print_log(reference, page_table, memory, frequency_table, @control_bits, frame_bits, page_bits, offset_bits)
-        process_reference(rest, memory, page_table, frequency_table, offset_bits, frame_bits, page_bits)
+        print_log(
+          reference,
+          page_table,
+          memory,
+          frequency_table,
+          @control_bits,
+          frame_bits,
+          page_bits,
+          offset_bits,
+          reference_list
+        )
+
+        process_reference(
+          rest,
+          memory,
+          page_table,
+          frequency_table,
+          offset_bits,
+          frame_bits,
+          page_bits
+        )
     end
   end
 
@@ -93,6 +110,7 @@ defmodule Simulation.Memory do
     case Enum.find_index(memory, fn x -> x == -1 end) do
       nil ->
         # Reemplazo por LFU: Encontrar el frame menos frecuentemente usado
+        IO.puts("Reemplazo por LFU")
         {lfu_frame, _frequency} =
           memory
           |> Enum.with_index()
@@ -112,8 +130,10 @@ defmodule Simulation.Memory do
 
         frequency_table =
           frequency_table
-          |> List.replace_at(reference, 1) # Nueva página comienza con frecuencia 1
-          |> List.replace_at(lfu_page, 0) # Página reemplazada, frecuencia a 0
+          # Nueva página comienza con frecuencia 1
+          |> List.replace_at(reference, 1)
+          # Página reemplazada, frecuencia a 0
+          |> List.replace_at(lfu_page, 0)
 
         {page_table, memory, frequency_table}
 
@@ -125,23 +145,81 @@ defmodule Simulation.Memory do
           |> set_frame_number(reference, memory_free_index)
           |> set_bits(reference, @present, frame_bits)
 
-        frequency_table = List.replace_at(frequency_table, reference, 1) # Frecuencia inicial 1
+        # Frecuencia inicial 1
+        frequency_table = List.replace_at(frequency_table, reference, 1)
 
         {page_table, memory, frequency_table}
     end
   end
 
-  defp print_log(reference, page_table, _memory, _frequency_table, control_bits, frame_bits, page_bits, offset_bits) do
-    IO.puts("\n\nReference: #{reference}")
-    print_virtual_physical_address(reference, page_table, control_bits, frame_bits, page_bits, offset_bits)
+  defp print_log(
+         reference,
+         page_table,
+         _memory,
+         frequency_table,
+         control_bits,
+         frame_bits,
+         page_bits,
+         offset_bits,
+        reference_list
+       ) do
+    IO.puts("Reference: #{reference}:\t\t\t time remaining: #{Enum.count(reference_list)}")
+
+    print_virtual_physical_address(
+      reference,
+      page_table,
+      control_bits,
+      frame_bits,
+      page_bits,
+      offset_bits
+    )
+
+    print_frame_number(page_table, reference, frame_bits)
+    print_control_bits(reference, page_table, control_bits, frame_bits, page_bits)
+    print_reference_count(reference, frequency_table)
+    IO.puts("\n\n")
   end
 
-  defp print_virtual_physical_address(reference, page_table, control_bits, frame_bits, page_bits, offset_bits) do
-    virtual_addr = reference <<< offset_bits
-    physical_addr = Enum.at(page_table, reference) |> Bitwise.band((1 <<< frame_bits) - 1) |> Bitwise.bsl(offset_bits)
+  defp print_frame_number(page_table, reference, frame_bits) do
+    frame_number = Enum.at(page_table, reference) |> Bitwise.band((1<<<frame_bits) - 1)
+    IO.puts("\n\tFrame Number")
+    IO.puts("\t\tFrame[#{reference}]: #{frame_number}")
+  end
 
-    IO.puts("\tVirtual  Address: #{virtual_addr} | 0b #{underscore_binary(virtual_addr, offset_bits)}")
-    IO.puts("\tPhysical Address: #{physical_addr} | 0b #{underscore_binary(physical_addr, offset_bits)}")
+  defp print_reference_count(reference, frequency_table) do
+    IO.puts("\n\tReference Count")
+    IO.puts("\t\tReference[#{reference}]: #{Enum.at(frequency_table, reference)}")
+  end
+
+  defp print_control_bits(reference, page_table, control_bits, frame_bits, page_bits) do
+    page_entry = Enum.at(page_table, reference)
+    control_bits = page_entry |> Bitwise.bsr(frame_bits) |> Integer.to_string(2) |> String.pad_leading(control_bits, "0")
+    IO.puts("\n\tControl Bits")
+    IO.puts( "\t\tControl[#{reference}]: #{control_bits}")
+  end
+
+  defp print_virtual_physical_address(
+         reference,
+         page_table,
+         control_bits,
+         frame_bits,
+         page_bits,
+         offset_bits
+       ) do
+    virtual_addr = reference <<< offset_bits
+
+    physical_addr =
+      Enum.at(page_table, reference)
+      |> Bitwise.band((1 <<< frame_bits) - 1)
+      |> Bitwise.bsl(offset_bits)
+
+    IO.puts(
+      "\tVirtual  Address: #{virtual_addr} | 0b #{underscore_binary(virtual_addr, offset_bits)}"
+    )
+
+    IO.puts(
+      "\tPhysical Address: #{physical_addr} | 0b #{underscore_binary(physical_addr, offset_bits)}"
+    )
   end
 
   defp underscore_binary(binary, offset_bits) do
@@ -169,7 +247,6 @@ defmodule Simulation.Memory do
       IO.puts("\t\tMemory[#{i}]: #{x} ")
     end)
   end
-
 
   defp increment_frequency(frequency_table, page_index) do
     List.update_at(frequency_table, page_index, &(&1 + 1))
