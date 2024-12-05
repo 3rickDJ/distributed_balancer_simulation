aa96a5fa7008a8836bdd7753f0f6c23880d3b9a2
3rickDJ <erick.dejesus1@alumno.buap.mx>
2024-12-05 10:44:57 -0600

    refactor: remove unused fields from Simulation.Program struct and update new function signature

--- lib/simulation/program.ex
+++ lib/simulation/program.ex
@@ -1,8 +1,6 @@
 defmodule Simulation.Program do
   defstruct references: [0, 4, 1, 4, 2, 4, 3, 4, 2, 4, 0, 4, 1, 4, 2, 4, 3, 4],
-            frames: 3,
             pages: 5,
-            page_size: 1024,
             name: "Generic Program",
             from_node: Node.self()
 
@@ -10,14 +8,12 @@ defmodule Simulation.Program do
     %__MODULE__{}
   end
 
-  def new(name, references, frames, pages, page_size) do
+  def new(name, references, pages) do
     %__MODULE__{
       name: "#{name}:#{Node.self()}",
       from_node: Node.self(),
       references: references,
-      frames: frames,
       pages: pages,
-      page_size: page_size
     }
   end
 end
